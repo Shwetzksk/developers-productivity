e084f6ef872b3b92f5b6278475ee79ded23ae15e
K Shweta Kumari <59056624+Shwetzksk@users.noreply.github.com>
2024-09-30 13:52:19 +0530

    removed not needed code

--- src/components/type.ts
+++
@@ -1,8 +0,0 @@
-export interface ChartData {
-  labels: Array<string>;
-  datasets: Array<{
-    label: string;
-    data: string[];
-    backgroundColor: string[] | string;
-  }>;
-}
--- src/service/get-team.ts
+++
@@ -1,7 +0,0 @@
-import fetchData from "./fetch-data";
-
-export default async function getTeam() {
-  const data = await fetchData();
-  const teams = data.rows.map((user) => user.name.split("@")[0]);
-  return teams;
-}
